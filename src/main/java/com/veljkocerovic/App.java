4c0cd5da4dd58054e45798c5f3f4b108313f6ef8
vcerovic <xvcerovic@gmail.com>
2022-07-02 16:51:09 +0200

    Initial commit

---
+++ src/main/java/com/veljkocerovic/App.java
@@ -0,0 +1,21 @@
+package com.veljkocerovic;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
