516d9c16cffa8b3f48e11b5ec84e26b32c1f7780
vcerovic <xvcerovic@gmail.com>
2022-07-02 17:27:49 +0200

    Created basic home view

--- src/main/java/com/veljkocerovic/App.java
+++ src/main/java/com/veljkocerovic/App.java
@@ -1,7 +1,13 @@
 package com.veljkocerovic;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * JavaFX App
@@ -9,7 +15,10 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
